d80762085e2a317b28dbdabf5e157d805b30b84b
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-02-16 23:22:05 +0900

    元に戻した

--- tweet.py
+++ tweet.py
@@ -11,5 +11,7 @@ params = {"status": tweet}
 
 req = twitter.post(
     "https://api.twitter.com/1.1/statuses/update.json", params=params)
-if req.status_code:
+if req.status_code == 200:
     print("Success!")
+else:
+    print("Error!")
